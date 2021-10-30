9096a79ce7fcaf343c538c46585d1f84459e57d3
Evan Payne <jpayne0061@gmail.com>
2021-10-30 09:34:43 -0400

    first commit

---
+++ PaintChaosDotNet5/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace PainChaos5.Controllers
+{
+    public class DataArray
+    {
+        public int[][] Data { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IHubContext<MessageHub> _hubContext;
+
+        public ValuesController(IHubContext<MessageHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        public async Task Post([FromBody] DataArray value)
+        {
+            await _hubContext.Clients.All.SendAsync("PAINT", value);
+        }
+    }
+}
---
+++ PaintChaosDotNet5/MessageHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace PainChaos5
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendMessage(string msg)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", msg);
+        }
+    }
+}
